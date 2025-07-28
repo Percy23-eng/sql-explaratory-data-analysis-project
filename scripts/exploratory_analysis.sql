105d5affc9561ba265889d74dac9bff1902e0d57
Percy23-eng <motsamai480@gmail.com>
2025-07-28 23:21:54 +0200

    Update exploratory_analysis.sql

--- scripts/exploratory_analysis.sql
+++ scripts/exploratory_analysis.sql
@@ -9,3 +9,22 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC (month,order_date)
 ORDER BY DATETRUNC (month,order_date)
+
+  
+/* Cumilitive Analysis
+Trying yo understand how the business is perfoming*/
+--Calculating Total Sales per month
+--Running total of sales over time
+SELECT
+order_date,
+total_sales,
+SUM(total_sales) OVER ( ORDER BY order_date) AS running_total_sales
+FROM
+(
+SELECT 
+DATETRUNC(month,order_date) AS order_date,
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month,order_date)
+)t
