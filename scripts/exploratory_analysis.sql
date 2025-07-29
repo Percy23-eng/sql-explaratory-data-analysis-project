9176b924a38c4417cd293ae5b65949ccfecee61e
Percy23-eng <motsamai480@gmail.com>
2025-07-29 22:35:23 +0200

    Update exploratory_analysis.sql

--- scripts/exploratory_analysis.sql
+++ scripts/exploratory_analysis.sql
@@ -10,6 +10,7 @@ WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC (month,order_date)
 ORDER BY DATETRUNC (month,order_date)
 
+
   
 /* Cumilitive Analysis
 Trying yo understand how the business is perfoming*/
@@ -28,3 +29,42 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC(month,order_date)
 )t
+
+
+  
+--Perfomance Analysis--
+/*Analyzing the yearly perfomance of products
+by comparing each products sales to both
+its average sales perfomance and the previous years sales
+*/
+
+WITH yearly_product_sales AS(
+SELECT 
+YEAR(f.order_date) AS order_year,
+p.product_name,
+SUM(f.sales_amount) AS current_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+WHERE f.order_date IS NOT NULL
+GROUP BY YEAR(f.order_date), p.product_name
+)
+
+SELECT 
+order_year,
+product_name,
+current_sales,
+AVG(current_sales) OVER (PARTITION BY product_name) avg_sales,
+current_sales - AVG(current_sales) OVER (PARTITION BY product_name) diff_avg,
+CASE WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above Avg'
+	WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'Below Avg'
+	ELSE 'AVG'
+	END avg_change,
+LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) py_sales, --Year Over Year Analysis
+current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) diff_py,
+CASE WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
+	WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
+	ELSE 'NO Change'
+END py_change
+FROM yearly_product_sales
+ORDER BY product_name, order_year
