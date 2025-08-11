ec0311593c9eaa3fcd9d6b8bd414e114bb18f21e
Percy23-eng <motsamai480@gmail.com>
2025-08-11 21:30:21 +0200

    Update exploratory_analysis.sql

--- scripts/exploratory_analysis.sql
+++ scripts/exploratory_analysis.sql
@@ -68,3 +68,21 @@ CASE WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORD
 END py_change
 FROM yearly_product_sales
 ORDER BY product_name, order_year
+
+
+--Which categories contribute to the most overall sales?
+WITH category_sales AS (
+SELECT
+category,
+SUM(sales_amount) total_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key 
+GROUP BY category)
+
+SELECT
+category,
+total_sales,
+SUM(total_sales) OVER() overall_sales,
+ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER()) * 100,2) percantage_of_total
+FROM category_sales
